d48df23cad4d4ace9b8312c027b6a9b70f4c2be3
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2025-05-14 15:33:49 -0300

    Ajustando sprites

--- scripts/characters/player/player.gd
+++ scripts/characters/player/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 class_name Player
 
 @export var speed = 100
+@export var gravity = 400
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
@@ -10,6 +11,7 @@ var is_attacking = false
 var direction = 0
 
 func _physics_process(delta):
+	_apply_gravity(delta)
 	_handle_input()
 	_handle_movement()
 	_handle_attack()
@@ -17,6 +19,10 @@ func _physics_process(delta):
 
 func _handle_input():
 	direction = Input.get_axis("move_left", "move_right")
+
+func _apply_gravity(delta):
+		if not is_on_floor():
+			velocity.y += gravity * delta
 
 func _handle_movement():
 	if not is_attacking:
@@ -27,6 +33,8 @@ func _handle_attack():
 	if Input.is_action_just_pressed("attack") and not is_attacking:
 		is_attacking = true
 		_play_animation("attack")
+		await get_tree().create_timer(0.3).timeout
+		is_attacking = false
 
 func _update_animation():
 	if is_attacking:
@@ -38,10 +46,6 @@ func _update_animation():
 	else:
 		_play_animation("idle")
 
-func _play_animation(name: String):
-	if animated_sprite_2d.animation != name:
-		animated_sprite_2d.play(name)
-
-func _on_animated_sprite_2d_animation_finished():
-	if animated_sprite_2d.animation == "attack":
-		is_attacking = false
+func _play_animation(animation_name):
+	if animated_sprite_2d.animation != animation_name:
+		animated_sprite_2d.play(animation_name)
